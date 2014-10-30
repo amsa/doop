82df8a0579c470feeff7379c7952d09ea7adbb6f
Wenbin Xiao <xwb1989@gmail.com>
2014-10-30 00:42:08 -0400

    comment out all in this file; we might not need to define result type at all because they are all defined in sql package

--- core/result.go
+++ core/result.go
@@ -1,14 +1,17 @@
 package core
 
-type Rows interface {
-	Next() bool
-	Scan(dest ...interface{}) error
-	Close() error
-	Columns() ([]string, error)
-	Err() error
-}
+/*
+We might not need this. Actually all result returned by driver should stick to the sql/rows or sql/result interface.
+*/
+//type Rows interface {
+//Next() bool
+//Scan(dest ...interface{}) error
+//Close() error
+//Columns() ([]string, error)
+//Err() error
+//}
 
-type Result interface {
-	LastInsertId() (int64, error)
-	RowsAffected() (int64, error)
-}
+//type Result interface {
+//LastInsertId() (int64, error)
+//RowsAffected() (int64, error)
+//}
