93cf454fd3d519dfa9ab81860c4c398a43a229df
amin <amin.w3dev@gmail.com>
2014-12-08 14:17:25 -0500

    removed unused files

--- core/install.go
+++ core/install.go
@@ -2,8 +2,9 @@ package core
 
 import (
 	"fmt"
-	. "github.com/amsa/doop/common"
 	"os"
+
+	. "github.com/amsa/doop/common"
 )
 
 func (doop *Doop) install() {
@@ -14,21 +15,4 @@ func (doop *Doop) install() {
 
 	Debug("Doop directory does not exist. Creating doop directory at %s...", doop.homeDir)
 	HandleError(os.Mkdir(doop.homeDir, 0755)) // Create Doop home directory
-
-	// Create configuration file
-	//defaultConfig := `[database]
-	//DSN = ` + dsn + `
-	//`
-	//handleError(ioutil.WriteFile(doop.getConfigFile(), []byte(defaultConfig), 0644))
-
-	// create database and needed tables
-	//adapter := adapter.GetAdapter(dsn)
-	//adapter.Exec(`CREATE TABLE doop_mapping (
-	//id integer NOT NULL PRIMARY KEY,
-	//dsn     text,
-	//db_name text,
-	//hash    text
-	//);`)
-	//adapter.Exec(`CREATE UNIQUE INDEX ON doop_mapping (hash);`)
-	//adapter.Close()
 }
